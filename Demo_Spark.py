7399772ca3bd8fcc6e43705b47865bb7b2047f31
RachelZeng0 <m15202119527@163.com>
2022-03-28 00:26:32 +0800

    demo testing commit

--- Demo_Spark.py
+++ Demo_Spark.py
@@ -1,6 +1,7 @@
+#code: utf8
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setAppName('Demo').setMaster('local[*]')
+conf = SparkConf().setAppName("de").setMaster("local[*]")
 sc = SparkContext(conf=conf)
 
 rdd = sc.parallelize(range(1,11))
